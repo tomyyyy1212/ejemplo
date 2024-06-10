dfe63b85c708a83b59be8d9eb7b31fb60ed32d18
tomyyyy1212 <tsantander@fen.uchile.cl>
2024-06-10 12:48:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify # type: ignore
+import json
+import pandas as pd
+import random
+import string
+import secrets
+from datetime import datetime
+import requests
+import mysql.connector
 
 app = Flask(__name__)
 
